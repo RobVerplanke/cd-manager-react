0211d77700958a7130f8ac8d8a70a8c2d0d3e9df
Rob <rverplanke@gmail.com>
2024-10-11 21:15:17 +0200

    feat: convert library option to accordion menu

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 function NavigationBar() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open); // Toggle the accordion navigation (Library option)
+  };
   return (
     <nav className="text-gray-300">
       <Link
@@ -17,43 +25,56 @@ function NavigationBar() {
       >
         Search
       </Link>
-      <button
-        aria-label="Open library sub-menu"
-        className="flex justify-center items-center h-9 w-full bg-slate-600 hover:bg-slate-500"
-      >
-        Library
-      </button>
-      <ul className="h-fit bg-slate-600">
-        <li>
-          <Link
-            to="/library/album"
-            aria-label="Go to album library"
-            className="flex justify-center items-center h-9 hover:bg-slate-500"
-          >
-            Albums
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/library/cd"
-            aria-label="Go to cd library"
-            className="flex justify-center items-center h-9 hover:bg-slate-500"
-          >
-            CDs
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/library/track"
-            aria-label="Co to track library"
-            className="flex justify-center items-center h-9 hover:bg-slate-500"
-          >
-            Tracks
-          </Link>
-        </li>
-      </ul>
+
+      {/* Accordion start */}
+      <div>
+        <button
+          onClick={handleOpen}
+          className="flex justify-center items-center h-9 bg-slate-600 hover:bg-slate-500 w-full"
+        >
+          Library
+          {open ? (
+            <ArrowDropUpIcon className="mt-1" />
+          ) : (
+            <ArrowDropDownIcon className="mt-1" />
+          )}
+        </button>
+        {open && (
+          <ul className="h-fit bg-slate-600">
+            <li>
+              <Link
+                to="/library/album"
+                aria-label="Go to album library"
+                className="flex justify-center items-center h-9 hover:bg-slate-500"
+              >
+                Albums
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/library/cd"
+                aria-label="Go to cd library"
+                className="flex justify-center items-center h-9 hover:bg-slate-500"
+              >
+                CDs
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/library/track"
+                aria-label="Go to track library"
+                className="flex justify-center items-center h-9 hover:bg-slate-500"
+              >
+                Tracks
+              </Link>
+            </li>
+          </ul>
+        )}
+      </div>
+      {/* Accordion end */}
+
       <Link
-        to="add"
+        to="/add"
         aria-label="Go to Add page"
         className="flex justify-center items-center h-9 bg-slate-600 hover:bg-slate-500"
       >
