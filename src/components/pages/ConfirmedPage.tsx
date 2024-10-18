5855f10bf500c884da2b980b380def643118e66c
Rob <rverplanke@gmail.com>
2024-10-18 22:35:34 +0200

    style: show message in centered box

--- src/components/pages/ConfirmedPage.tsx
+++ src/components/pages/ConfirmedPage.tsx
@@ -1,7 +1,17 @@
 function ConfirmedPage({ action }: { action: string }) {
   return (
-    <div className="p-6">
-      <h2>Item {action}!</h2>
+    <div className="flex justify-center pt-60 h-dvh p-6">
+      <div className="flex justify-center items-center w-64 h-20 border-2 border-[#48CFCB]">
+        <p className="text-2xl pr-2">Item {action}</p>
+        <div>
+          <img
+            src="../src/assets/tick-mark-icon.png"
+            alt=""
+            width="26"
+            height="26"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/pages/ErrorPage.tsx
+++ src/components/pages/ErrorPage.tsx
@@ -1,7 +1,17 @@
 function ErrorPage({ error }: { error: string }) {
   return (
-    <div className="p-6">
-      <h2>Something went wrong: {error}</h2>{' '}
+    <div className="flex justify-center pt-60 h-dvh p-6">
+      <div className="flex justify-center items-center w-auto h-20">
+        <div>
+          <img
+            src="../src/assets/cross-icon.png"
+            alt=""
+            width="26"
+            height="26"
+          />
+        </div>
+        <p className="text-xl px-2">Something went wrong: {error}</p>
+      </div>
     </div>
   );
 }
