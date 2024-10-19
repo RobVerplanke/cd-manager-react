fd86163ce510e2630f76ba35b3a18cbce4805444
Rob <rverplanke@gmail.com>
2024-10-19 17:51:43 +0200

    style: change amount of items per page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
-export const MAX_AMOUNT_TRACKS_PER_PAGE = 5;
-export const MAX_AMOUNT_CDS_PER_PAGE = 5;
+export const MAX_AMOUNT_TRACKS_PER_PAGE = 8;
+export const MAX_AMOUNT_CDS_PER_PAGE = 8;
 export const MAX_AMOUNT_RATING_STARS = 5;
 export const RATING_VALUES = [0, 1, 2, 3, 4, 5];
 export const MESSAGE_TIMEOUT_SECONDS = 3000;
